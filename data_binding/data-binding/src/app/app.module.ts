24e813c11c2d0894bd3dfcd9e3495bbc6fd8b859
Luis <hsantos.luis@outlook.com>
2022-02-21 23:47:56 -0300

    Aula sobre data binding

--- data_binding/data-binding/src/app/app.module.ts
+++ data_binding/data-binding/src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataBindingComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DataBindingComponent],
+  imports: [BrowserModule, MeuFormModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
