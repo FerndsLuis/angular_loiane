24610b585ac63b032b34f86cc912dff0e5d0bc8f
Luis <hsantos.luis@outlook.com>
2022-02-24 23:44:47 -0300

    Ajuste - data-bind

--- data_binding/data-binding/src/app/app.module.ts
+++ data_binding/data-binding/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, DataBindingComponent],
-  imports: [BrowserModule, MeuFormModule],
+  imports: [BrowserModule, FormsModule, MeuFormModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
