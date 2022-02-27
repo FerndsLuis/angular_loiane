23157f15d4b78d4ef25607b0a2613aee36ebf838
Luis <hsantos.luis@outlook.com>
2022-02-27 15:30:14 -0300

    Output e eventos

--- data_binding/input-properties/src/app/app.module.ts
+++ data_binding/input-properties/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { InputPropertyComponent } from './input-property/input-property.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { InputPropertyModule } from './input-property/input-property.module';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
-  declarations: [AppComponent, DataBindingComponent],
+  declarations: [AppComponent, DataBindingComponent, OutputPropertyComponent],
   imports: [BrowserModule, NgbModule, InputPropertyModule],
   providers: [],
   bootstrap: [AppComponent],
