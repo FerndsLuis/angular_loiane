6978577a6e8d5e525d636e64f1c7e9c861e05b51
Luis <hsantos.luis@outlook.com>
2022-03-13 22:48:32 -0300

    ng-content

--- diretiva/diretivas/src/app/app.module.ts
+++ diretiva/diretivas/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.co
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { FormsModule } from '@angular/forms';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
     DiretivaNgclassComponent,
     DiretivaNgstyleComponent,
     OperadorElvisComponent,
+    ExemploNgContentComponent,
   ],
   imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
