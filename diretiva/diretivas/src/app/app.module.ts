573197f466eadd515f372d1e78c915f76641b7d6
Luis <hsantos.luis@outlook.com>
2022-03-13 21:30:27 -0300

    ngClass

--- diretiva/diretivas/src/app/app.module.ts
+++ diretiva/diretivas/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaSwitchComponent } from './diretiva-switch/diretiva-switch.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaSwitchComponent,
-    DiretivaNgforComponent
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule,
